036370eb09f78163aedba9d7d87ee7560dd87975
Luis <luis@airbnb-cleaning.local>
2026-09-10 16:21:04 -0300

    fix: corrigir cadeia de migrations LGPD (evitar multiple heads)

--- backend/alembic/versions/a1b2c3d4e5f6_add_lgpd_compliance_fields.py
+++ backend/alembic/versions/a1b2c3d4e5f6_add_lgpd_compliance_fields.py
@@ -1,7 +1,7 @@
 """add lgpd compliance fields
 
 Revision ID: a1b2c3d4e5f6
-Revises: ffc35eda2650
+Revises: 531e24bb7bde
 Create Date: 2026-09-10
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = 'ffc35eda2650'
+down_revision: Union[str, None] = '531e24bb7bde'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
